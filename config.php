b33d19a46250b554e18f2b084a0db7def9aee6ea
Kushan <kushan.mehta02@gmail.com>
2017-07-04 17:48:10 +0530

    Update config file with detailed documentation

--- config.php
+++ config.php
@@ -9,12 +9,12 @@ define("COMPANY_NAME", "[Company Name, LLC]"); // The name of you company (eg. G
 define("ADDR_LINE_1", "1234 Street Rd."); // Line 1 of your address
 define("ADDR_LINE_2", "Suite 1234"); // Line 2 of your address
 
-define("EMAIL_ID_FROM","webmaster@kushan02.xyz"); // The email-id of your domain which would be seen in from field
+define("EMAIL_ID_FROM","webmaster@yoursite.com"); // The email-id of your domain which would be seen in from field
 
 
-define("RESET_PAGE_LOCATION", "http://kushan:kushan@client.kushan02.xyz/hostinger/forgot-pass/reset.php"); // Link to file on your website where reset.php is stored
+define("RESET_PAGE_LOCATION", "http://yoursite.com/reset.php"); // Link to file on your website where reset.php is stored
 
-define("EMAIL_PAGE_LOCATION", "http://kushan:kushan@client.kushan02.xyz/hostinger/forgot-pass/email.php"); // Link to file on your website where email.php is stored
+define("EMAIL_PAGE_LOCATION", "http://yoursite.com/email.php"); // Link to file on your website where email.php is stored
 
 
 /* Here 'users' is the name of table where registered users are stored
@@ -22,7 +22,7 @@ email - table column where email address of registered users are stored
 */
 function sqlf($email)
 {
-    $sql = "SELECT * FROM users WHERE email='$email'";
+    $sql = "SELECT * FROM users WHERE email='$email'"; // This query selects the user email from 'users' table
     return $sql;
 
 }
@@ -33,7 +33,7 @@ function sqlf($email)
 */
 function sqlonef($email,$token,$expiry)
 {
-    $sqlone =  "INSERT INTO forgot_pass (email,token,expiry)VALUE ('$email','$token','$expiry')";
+    $sqlone =  "INSERT INTO forgot_pass (email,token,expiry)VALUE ('$email','$token','$expiry')"; // SQL query to insert a new token request into the table 'forgot_pas'
     return $sqlone;
 }
 
@@ -56,7 +56,7 @@ pass - table column where password encrypted using sha-512 are stored
 */
 function sqlresetf($email,$password)
 {
-    $sqlreset =  "UPDATE users SET pass='$password' WHERE email='$email'";
+    $sqlreset =  "UPDATE users SET pass='$password' WHERE email='$email'"; // SQL Code to update the current password to new password in 'users' table
 
     return $sqlreset;
 }
@@ -69,7 +69,7 @@ function sqlresetf($email,$password)
 */
 function sqldelf($email,$token)
 {
-    $sqldel = "DELETE FROM forgot_password WHERE email='$email' AND token='$token'";
+    $sqldel = "DELETE FROM forgot_password WHERE email='$email' AND token='$token'"; // SQL Code to delete the token entry from table once the user resets his password
 
     return $sqldel;
 }
